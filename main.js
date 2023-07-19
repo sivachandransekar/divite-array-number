82bae58eb28f948d7a7a786c171962a9a892832a
siva <sivasebi4545@gmail.com>
2023-07-19 16:47:23 +0530

    divide the number

---
+++ main.js
@@ -0,0 +1,12 @@
+var arr=[10,77,41,15,18,27,99];
+
+document.write("Giver the array Element="+arr+"<br>");
+document.write(" divide the 3,5 number is"+"<br>");
+for(let i=0;i<arr.length;i++){
+	if((arr[i]%3==0)&&(arr[i]%5==0))
+	{
+		
+ document.write( arr[i]+"<br>");
+ 
+}
+}
